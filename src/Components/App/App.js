26af5970d16ed3674d2561836878245506052b0b
Erin Black <erin.black@fieldnation.com>
2018-06-07 13:47:39 -0500

    added city value to form

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,21 +6,28 @@ class App extends Component {
 
     this.state ={
       user: '',
+      city: '',
     };
     // Makes this in handle change the same as this in constructor
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeCity = this.handleChangeCity.bind(this);
   }
 
   handleChange(event){
     console.log(event.target.value);
     this.setState({user: event.target.value});
+  }
 
+  handleChangeCity(event){
+    this.setState({city: event.target.value});
   }
   render() {
     return (
       <div>
         <p>This user is: {this.state.user}</p>
         <p><input onChange={this.handleChange}/></p>
+        <p>This city is: {this.state.city}</p>
+        <p><input onChange={this.handleChangeCity}/></p>
       </div>
     );
   }
