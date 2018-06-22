ca333a63db202551d602b0552bd9dcb805abca1d
Erin Black <erin.black@fieldnation.com>
2018-06-22 13:20:52 -0500

    changed div into form and cleared input fields on submit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,38 +12,38 @@ class App extends Component {
         zipCode: '',
       },
     };
-    // Makes this in handle change the same as this in constructor
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChangeCity = this.handleChangeCity.bind(this);
-  }
- 
-  handleChange(event){
-    console.log(event.target.value);
-    this.setState(
-      {user: {
-        ...this.state.user,
-        name: event.target.value,
-      }
-    });
   }
 
-  handleChangeCity(event){
-    this.setState(
-      {user: {
-        ...this.state.user,
-        city: event.target.value
-      }
-    });
+  // Currying
+  handleChangeFor = propertyName => event => {
+      this.setState({
+        user: {
+          ...this.state.user,
+         [propertyName]: event.target.value,
+        }
+      })
   }
+
+  // Logging 
+  handleClick = (e) => {
+    console.log('in handleclick')
+    console.log('name', this.state.user.name);
+    console.log('city', this.state.user.city);
+    e.target.reset();
+
+  }
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleClick}>
         <label for="name">What is your name?</label>
-        <p><input id="name" onChange={this.handleChange}/></p>
+        <p><input id="name" onChange={this.handleChangeFor('name')}/></p>
         <label for="City">What city do you live in? </label>
-        <p><input id="city" onChange={this.handleChangeCity}/></p>
+        <p><input id="city" onChange={this.handleChangeFor('city')}/></p>
+  
+        <button >Log the Object</button>
         <p>{this.state.user.name} is from: {this.state.user.city}</p>
-      </div>
+      </form>
     );
   }
 }
