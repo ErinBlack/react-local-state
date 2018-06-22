ca0851061fda4d77cd8dc0dc4b3c5d5266d44cfb
Erin Black <erin.black@fieldnation.com>
2018-06-22 11:58:55 -0500

    added spread operator in order to make code work better

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,21 +5,35 @@ class App extends Component {
     super(props);
 
     this.state ={
-      user: '',
-      city: '',
+      user: {
+        name: '',
+        city: '',
+        state: '',
+        zipCode: '',
+      },
     };
     // Makes this in handle change the same as this in constructor
     this.handleChange = this.handleChange.bind(this);
     this.handleChangeCity = this.handleChangeCity.bind(this);
   }
-
+ 
   handleChange(event){
     console.log(event.target.value);
-    this.setState({user: event.target.value});
+    this.setState(
+      {user: {
+        ...this.state.user,
+        name: event.target.value,
+      }
+    });
   }
 
   handleChangeCity(event){
-    this.setState({city: event.target.value});
+    this.setState(
+      {user: {
+        ...this.state.user,
+        city: event.target.value
+      }
+    });
   }
   render() {
     return (
@@ -28,7 +42,7 @@ class App extends Component {
         <p><input id="name" onChange={this.handleChange}/></p>
         <label for="City">What city do you live in? </label>
         <p><input id="city" onChange={this.handleChangeCity}/></p>
-        <p>{this.state.user} is from: {this.state.city}</p>
+        <p>{this.state.user.name} is from: {this.state.user.city}</p>
       </div>
     );
   }
